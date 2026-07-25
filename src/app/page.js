7c2560cdc60647dc7efc05fb0a20c9e0b386d944
SimC0de <yor1simone@outlook.com>
2026-07-25 21:53:43 +0800

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,34 @@
 import Image from "next/image";
+import { Laptop, CodeXml, Video, Layers } from 'lucide-react';
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div>
+      <header className="p-15 border-b-2 border-b-gray-500">
+      </header>
+      <div className="flex flex-col py-10 px-50 gap-5">
+        <div className="flex flex-col items-center gap-3 p-10">
+          <Image alt="profile_picture" src="/profile_picture.png" width="200" height="200" className="rounded-[50%]" />
+          <h1 className="text-4xl font-bold">Simone Roy G. Abello</h1>
+          <ul className="flex text-[20px] text-gray-500 gap-5">
+            <li className="flex flex-col items-center text-center"><Laptop /> IT Graduate</li>
+            <li>|<br />|</li>
+            <li className="flex flex-col items-center text-center"><Layers /> Full-Stack Web Developer</li>
+            <li>|<br />|</li>
+            <li className="flex flex-col items-center text-center"><Video /> Video Editor</li>
+          </ul>
+          <p className="text-[18px] text-justify">Hi, I&apos;m Simone Roy, a fresh Information Technology graduate with sufficient knowledge of web development and programming and hands-on experience with video editing. Dedicated to continuous learning and professional growth.</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex flex-col gap-5 border border-[#222222] rounded-xl bg-[#111111] py-5 px-10">
+          <h1 className="text-3xl font-bold">Certificates</h1>
+          <div className="flex gap-5">
+            <Image alt="CodeCademy Javascript Certificate" src="/certificates/cc_learn_js_cert.png" width="350" height="276" />
+            <Image alt="CodeCademy Javascript Certificate" src="/certificates/cc_learn_py_cert.png" width="350" height="276" />
+            <Image alt="CodeCademy Javascript Certificate" src="/certificates/cc_learn_sql_cert.png" width="350" height="276" />
+            <Image alt="CodeCademy Javascript Certificate" src="/certificates/cc_learn_php_cert.png" width="350" height="276" />
+          </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
